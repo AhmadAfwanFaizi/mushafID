c94321e259fe96e966e3cd33629349a02b194b38
AhmadAfwanFaizi <ahmadafwanfaizi@gmail.com>
2023-07-14 15:23:53 +0700

    nav bookmark

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,10 @@ const Navbar = () => {
   const location = useLocation();
   const path = location.pathname.split("/");
 
-  return (
-    <div
-      className="fixed bg-romance top-0 z-10 flex justify-between py-4 text-2xl font-semibold"
-      style={{ width: "inherit" }}
-    >
-      {/* left */}
-      <div className="left">
-        {path[1] === "" ? (
+  const navLeft = (pathParam) => {
+    switch (pathParam) {
+      case "":
+        return (
           <Link to="/" className="btn btn-ghost">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -28,7 +24,11 @@ const Navbar = () => {
               />
             </svg>
           </Link>
-        ) : path[1] === "quran" ? (
+        );
+        break;
+
+      case "quran":
+        return (
           <Link to="/" className="btn btn-ghost">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -45,7 +45,10 @@ const Navbar = () => {
               />
             </svg>
           </Link>
-        ) : path[1] === "surah" ? (
+        );
+        break;
+      case "surah":
+        return (
           <Link to="/quran" className="btn btn-ghost">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -62,19 +65,38 @@ const Navbar = () => {
               />
             </svg>
           </Link>
-        ) : (
-          ""
-        )}
-      </div>
+        );
+        break;
+      case "bookmark":
+        return (
+          <Link to="/" className="btn btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={3}
+              stroke="currentColor"
+              className="w-7 h-7"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+              />
+            </svg>
+          </Link>
+        );
+        break;
+      default:
+        return "";
+        break;
+    }
+  };
 
-      {/* center */}
-      <div className="center m-auto">
-        <h1>MyQuran</h1>
-      </div>
-
-      {/* right */}
-      <div className="right">
-        {path[1] === "surah" ? (
+  const navRight = (pathParam) => {
+    switch (pathParam) {
+      case "surah":
+        return (
           <Link to={"/"} className="btn btn-ghost">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -91,10 +113,30 @@ const Navbar = () => {
               />
             </svg>
           </Link>
-        ) : (
-          ""
-        )}
+        );
+        break;
+
+      case "":
+        return "";
+        break;
+    }
+  };
+
+  return (
+    <div
+      className="fixed bg-romance top-0 z-10 flex justify-between py-4 text-2xl font-semibold"
+      style={{ width: "inherit" }}
+    >
+      {/* left */}
+      <div className="left">{navLeft(path[1])}</div>
+
+      {/* center */}
+      <div className="center m-auto">
+        <h1>MyQuran</h1>
       </div>
+
+      {/* right */}
+      <div className="right">{navRight(path[1])}</div>
     </div>
   );
 };
